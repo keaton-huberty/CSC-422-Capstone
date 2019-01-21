f5a3ef5e5e930e334933cfa32923ccb20b24a250
keaton-huberty <30304328+keaton-huberty@users.noreply.github.com>
2019-01-21 11:23:43 -0600

    Initial commit

---
+++ src/capstoneweek1/CapstoneWeek1.java
@@ -0,0 +1,15 @@
+/*
+This is a place holder to put into the capstone repository on our GitHub
+ */
+package capstoneweek1;
+
+/**
+Keaton, Will, Mike, and Amin
+1/21/2019
+ */
+public class CapstoneWeek1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+    
+}
