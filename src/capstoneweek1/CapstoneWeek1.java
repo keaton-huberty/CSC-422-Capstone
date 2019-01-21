60886b7c6c7baa431680eaaad9c4b4db34510142
willnardo <bensoncope@gmail.com>
2019-01-21 17:12:50 -0600

    will test commit

--- src/capstoneweek1/CapstoneWeek1.java
+++ src/capstoneweek1/CapstoneWeek1.java
@@ -11,6 +11,8 @@ public class CapstoneWeek1 {
     public static void main(String[] args) {
         System.out.println("Hello World! This will become Keaton, Will, Mike"
                 + "and Amin's Capstone Project for CSC 422.");
+        
+        System.out.println("This is Will making a change to the test file.  Cheers!");
     }
     
 }
