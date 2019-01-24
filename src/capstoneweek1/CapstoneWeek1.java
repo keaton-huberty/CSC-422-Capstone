1fca7cdc4b9cf96b3cc72b48f9f82a36da1b428e
willnardo <bensoncope@gmail.com>
2019-01-23 18:29:26 -0600

    added login screen - need to use localhost DB

--- src/capstoneweek1/CapstoneWeek1.java
+++ src/capstoneweek1/CapstoneWeek1.java
@@ -1,21 +1,102 @@
 /*
-This is a place holder to put into the capstone repository on our GitHub
+Capstone Team 1n 
  */
 package capstoneweek1;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
-Keaton, Will, Mike, and Amin
-1/21/2019
+ * Keaton, Will, Mike, and Amin 1/21/2019
+ * 
  */
-public class CapstoneWeek1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World! This will become Keaton, Will, Mike"
-                + "and Amin's Capstone Project for CSC 422.");
-        
-        System.out.println("This is Will making a change to the test file.  Cheers!");
-        
-        // new comment to test push - will
-        System.out.println("test new comment");
-    }
+public class CapstoneWeek1 extends Application {
+
+    private final TextField tfUsername = new TextField();
+    private final TextField tfPassword = new TextField();
+    private final Label lbUsername = new Label("Username");
+    private final Label lbPassword = new Label("Password");
+    private final Button btnLogin = new Button("Login");
     
+    // New comment to test pushing to GitHub
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Datbase Utility object used for checking login credentials
+        // This database is connected to my localhost:3306.  I believe its the default port 
+        // when setting up a database locally using phpmyadmin
+        DBUtility db = new DBUtility();
+
+        //The vertical Box holds the text fields, labels, and the log in button
+        VBox vBox = new VBox();
+        VBox vBoxDashboard = new VBox();
+        
+        // adding the labesl, textfields and button to the vertical box
+        vBox.getChildren().addAll(tfUsername, lbUsername, tfPassword, lbPassword, btnLogin);
+        vBox.setAlignment(Pos.CENTER);
+
+        // Adding the vertical box to the scene
+        Scene scene = new Scene(vBox, 250, 350);
+
+        
+        // this is the listener that calls the function to check if the username matches the password
+        // if correct, the window will close and a new window will open that will be the user's dashboard
+        btnLogin.setOnAction((javafx.event.ActionEvent e) -> {
+
+            // connecting to the database
+            try {
+                db.dbConnect();
+            } catch (SQLException ex) {
+                Logger.getLogger(CapstoneWeek1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            // This method, "checkLogin", takes the text entered in the username and password field and passes
+            // them along as strings.  Click over to the DBUtility class to see the method.
+            // It is a boolean.  If returns true, it opens a new window, the future dashboard
+            // if false, it will pop up with a error message (not complete)
+            try {
+                if (db.checkLogin(tfUsername.getText(), tfPassword.getText())) {
+
+                    Scene dashboard = new Scene(vBoxDashboard, 600, 400);
+                    Stage dashboardStage = new Stage();
+                    dashboardStage.setTitle("Welcome " + tfUsername.getText() + "!");
+                    dashboardStage.setScene(dashboard);
+                    primaryStage.close();
+                    dashboardStage.show();
+                } else {
+                    System.out.println("Wrong password or username");
+
+                    Scene loginErrorScene = new Scene(vBoxDashboard, 600, 400);
+                    Stage loginErrorStage = new Stage();
+                    loginErrorStage.setTitle("WRONG USERNAME OR PASSWORD!");
+                    loginErrorStage.setScene(loginErrorScene);
+                    loginErrorStage.show();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(CapstoneWeek1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        });
+
+        primaryStage.setTitle("Social Gaming Pro!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
 }
---
+++ src/capstoneweek1/DBUtility.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstoneweek1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Will
+ * This is the database connection utility
+ */
+public class DBUtility {
+
+    // creating class variables used to connect to the database.
+    // watch this youtube video to add the "connector" - https://www.youtube.com/watch?v=nW13FmTdkjc
+    private Connection conn = null;
+    private Statement stmt = null;
+    private ResultSet resultSet = null;
+
+    // dbConnect will connect to the database on the local host
+    public void dbConnect() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Capstone2019", "root", "mysql");
+        Statement stmt = null;
+    }
+
+    // checkLogin will take the username and password entered as strings
+    // I'm not super great at SQL but I got this to work so far.
+    public boolean checkLogin(String inputUserName, String inputPassword) throws SQLException {
+        String username = null;
+        String password = null;
+        
+        //first have to creat a statement
+        stmt = conn.createStatement();
+        // this runs the SQL query - notice the extra single quotes around the string.  Don't forget those.
+        resultSet = stmt.executeQuery("SELECT * FROM userLogin WHERE userName = '" + inputUserName + "'");
+
+        //In order to read what the query returns, we have to use resultSet.next(), otherwise we get an error
+        //Here I am assigning the correct password with the username
+        if (resultSet.next()) {
+            username = resultSet.getString("userName");
+            password = resultSet.getString("userPassword");
+        } else {
+            System.out.println("Username does not exist!");
+        }
+
+        //checking to see if the user entered password equals the password stored in the database
+        return inputPassword.equals(password);
+
+    }
+    
+    // constructor
+    public DBUtility(){};
+
+}
